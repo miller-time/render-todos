ea768d9f80a5535a7fc517445ebb4c3756f32a2b
Russell <miller.time.baby@gmail.com>
2025-06-04 22:01:49 -0700

    add /todos endpoint

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,12 +1,35 @@
 #[macro_use]
 extern crate rocket;
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world"
+use rocket::serde::json::Json;
+use serde::Serialize;
+
+#[derive(Debug, Serialize)]
+struct Todo {
+    text: String,
+    completed: bool,
+}
+
+#[get("/todos")]
+fn list_todos() -> Json<Vec<Todo>> {
+    let todos = vec![
+        Todo {
+            text: String::from("implement api"),
+            completed: true,
+        },
+        Todo {
+            text: String::from("connect client to api"),
+            completed: false,
+        },
+        Todo {
+            text: String::from("connect api to db"),
+            completed: false,
+        },
+    ];
+    Json(todos)
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/", routes![list_todos])
 }
