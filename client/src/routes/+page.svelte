9a19711590aaeb6636a07790851803c89a9a176e
Russell <miller.time.baby@gmail.com>
2025-06-04 22:47:43 -0700

    fetch todos from api

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+  type Todo = { text: string; completed: boolean };
+  const promise: Promise<Todo[]> = fetch(
+    "https://todos-api-u0so.onrender.com/todos",
+  ).then((response) => response.json());
+</script>
+
+<h1>To Do</h1>
+{#await promise then todos}
+  <ul>
+    {#each todos as todo}
+      <li>
+        {#if todo.completed}
+          <s>{todo.text}</s>
+        {:else}
+          <span>{todo.text}</span>
+        {/if}
+      </li>
+    {/each}
+  </ul>
+{/await}
